633a2f46beeb8914e19235505d7cfe7ab777ddf9
fabiano <fabianopaiva@unb.br>
2020-07-06 16:46:36 -0300

    Instalar e usar modulo terceiro Morgan como logger

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const dotenv = require('dotenv');
 //Route files
 const bootcamps = require('./routes/bootcamp');
-const logger = require('./middleware/logger');
+const morgan = require('morgan');
 
 //Load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-app.use(logger);
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
