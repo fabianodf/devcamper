7ad81b9b5aa1ed67c8e94f76e2e71f05f13cacae
fabiano <fabianopaiva@unb.br>
2020-07-06 19:26:25 -0300

    Configura conexao com MongoDB

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
-//Route files
-const bootcamps = require('./routes/bootcamp');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 //Load env vars
 dotenv.config({ path: './config/config.env' });
+
+//Connect to database
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamp');
 
 const app = express();
 
@@ -18,4 +23,11 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running on ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running on ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// Handle unhandle promise exception
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    //Close server and exit proccess
+    server.close(() => process.exit(1));
+})
