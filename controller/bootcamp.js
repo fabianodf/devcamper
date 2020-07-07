17b592820b8f094e3d575334303f6c0a5d4e7815
fabiano <fabianopaiva@unb.br>
2020-07-07 11:17:20 -0300

    Parte R do CRUD, ler todos atraves de GET e usando findById quando passa id como parametro

--- controller/bootcamp.js
+++ controller/bootcamp.js
@@ -3,19 +3,30 @@ const Bootcamp = require('../models/Bootcamp');
 //@desc     Recupera todos os bootcamps
 //@route    GET /api/v1/bootcamps
 //access    public 
-exports.getBootcamps = (req, res, next) => {
-    res.
-        status(200).
-        json({ success: true, msg: "Show all bootcamps" });
+exports.getBootcamps = async (req, res, next) => {
+    try {
+        const bootcamps = await Bootcamp.find();
+        res.status(200).json({ success: true, data: bootcamps });
+    } catch (err) {
+        res.status(400).json({ success: false });
+    }
 }
 
 //@desc     Recupera unico bootcamp
 //@route    GET /api/v1/bootcamps/:id
 //access    public 
-exports.getBootcamp = (req, res, next) => {
-    res.
-        status(200).
-        json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+exports.getBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+
+        if (!bootcamp) {
+            return res.status(400).json({ success: false });
+        }
+
+        res.status(200).json({ success: true, data: bootcamp });
+    } catch (err) {
+        res.status(400).json({ success: false });
+    }
 }
 
 //@desc     Create bootcamp
