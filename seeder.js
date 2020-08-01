581eac62d56bd15c8038d39429c5d0a50a94f904
Fabiano Rodrigues Paiva <fabianopaiva@unb.br>
2020-08-01 19:41:02 -0300

    Seeder para inserir e deletar dados de bootcamps

---
+++ seeder.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const path = require('path');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const colors = require('colors');
+
+dotenv.config({ path: './config/config.env' });
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+});
+
+const bootcamps = JSON.parse(
+    fs.readFileSync(path.join(__dirname, '_data', 'bootcamps.json'), 'utf-8')
+);
+
+const Bootcamp = require('./models/Bootcamp');
+
+const importData = async () => {
+    try {
+        await Bootcamp.create(bootcamps);
+        console.log('Data imported...'.green.inverse);
+        process.exit();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const deleteData = async () => {
+    try {
+        await Bootcamp.deleteMany();
+        console.log('Data destroyed...'.red.inverse);
+        process.exit();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+if (process.argv[2] === '-i') {
+    importData();
+} else if (process.argv[2] === '-d') {
+    deleteData();
+} else {
+    console.log("Please add valid options for the seeder -i or -d");
+    process.exit();
+}
+
