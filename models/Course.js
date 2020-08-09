3d4973351fce8ed1e91ac8e51b348cffe7f8f28a
Fabiano Rodrigues Paiva <fabianopaiva@unb.br>
2020-08-09 17:26:26 -0300

    Usa Agreggate do mongoose para calcular o avgCost dos cursos de um bootcamp. (tipo GroupBy)

--- models/Course.js
+++ models/Course.js
@@ -39,4 +39,38 @@ const CourseSchema = new mongoose.Schema({
     }
 });
 
+// Static method to get avg of course tuitions
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+    console.log('Calculating avg cost...'.blue);
+
+    const obj = await this.aggregate([
+        {
+            $match: { bootcamp: bootcampId }
+        },
+        {
+            $group: {
+                _id: '$bootcamp',
+                averageCost: { $avg: "$tuition" }
+            }
+        }
+    ]);
+
+    try {
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+            averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+CourseSchema.post('save', async function () {
+    await this.constructor.getAverageCost(this.bootcamp);
+
+});
+
+CourseSchema.post('remove', async function () {
+    await this.constructor.getAverageCost(this.bootcamp);
+});
+
 module.exports = mongoose.model('Course', CourseSchema);
