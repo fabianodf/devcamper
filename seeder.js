f315f78c281b29f575c4fe2089e4fe6b45c5df17
Fabiano Rodrigues Paiva <fabianopaiva@unb.br>
2020-08-21 17:36:57 -0300

    Cria relacionamento entre User e Bootcamp, vincula user ao bootamp no momento de criacao do Bootcamp + adiciona User ao seeder

--- seeder.js
+++ seeder.js
@@ -21,13 +21,19 @@ const courses = JSON.parse(
     fs.readFileSync(path.join(__dirname, '_data', 'courses.json'), 'utf-8')
 );
 
+const users = JSON.parse(
+    fs.readFileSync(path.join(__dirname, '_data', 'users.json'), 'utf-8')
+);
+
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
+const User = require('./models/User');
 
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
         await Course.create(courses);
+        await User.create(users);
         console.log('Data imported...'.green.inverse);
         process.exit();
     } catch (error) {
@@ -39,6 +45,7 @@ const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
         await Course.deleteMany();
+        await User.deleteMany();
         console.log('Data destroyed...'.red.inverse);
         process.exit();
     } catch (error) {
